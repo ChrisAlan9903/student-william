ca7eae390a2b005c859644b609ee3d079c082531
ChristopherAhlim <118709622+ChrisAlan9903@users.noreply.github.com>
2026-07-05 18:05:23 +0800

    feat: lesson 2 math

---
+++ JS-2/Tutor/Lesson-JS-2.js
@@ -0,0 +1,24 @@
+/* Lesson 2: Operators
+    Types of operators: 
+        1. Math operator
+        2. Comparison operator
+        3. Logical operator
+*/
+
+// 2.a: Math operator
+//  + , - , * , / , % , ()
+let triangle = 20 * 10 * (1 / 2);
+// console.log(triangle);
+
+let width = 10;
+let height = 30;
+const pi = 3.142;
+
+let rectangle = width * height;
+// console.log(rectangle);
+
+console.log(10 % 1);
+console.log(10 % 2);
+console.log(10 % 3);
+console.log(10 % 4);
+console.log(10 % 5);
